d345fd491ebc1e561ef24474a1967a0ada8095aa
Cristian Perez <Cpere20@illinois.edu>
2023-03-24 14:15:46 -0500

    Testing UI

---
+++ testUI.py
@@ -0,0 +1,19 @@
+from tkinter import *
+root = Tk()
+root.title("welcome")
+root.geometry('350x200')
+lbl = Label(root, text="random")
+lbl.grid()
+
+
+def clicked():
+    btn['state'] = 'disabled'
+    btn['text'] = 'blank'
+    btn['bg'] = 'white'
+    lbl2 = Label(root, text="Thank you")
+    lbl2.grid()
+
+
+btn = Button(root, text="blank", fg="white", bg="red", command=clicked)
+btn.grid(column=1, row=0)
+root.mainloop()
