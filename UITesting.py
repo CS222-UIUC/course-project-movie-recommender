fedb369802f6ef2d2269c9b238907976d62b8edf
Cristian Perez <Cpere20@illinois.edu>
2023-03-31 14:25:01 -0500

    Created a new title and new background for UI

--- UITesting.py
+++ UITesting.py
@@ -1,9 +1,12 @@
 from tkinter import *
+import tkinter as tk
 root = Tk()
-root.title("welcome")
+root.title("Movie Recommender")
 root.geometry('350x200')
-lbl = Label(root, text="random")
+lbl = Label(root, text="Personalized Movie Recommender")
 lbl.grid()
+
+# creating a commmened for when button is clicked
 
 
 def clicked():
@@ -13,6 +16,9 @@ def clicked():
     lbl2 = Label(root, text="Test Label")
     lbl2.grid()
 
+
+# changes backgound color
+root.configure(bg='lightblue')
 
 btn = Button(root, text="blank", fg="white", bg="red", command=clicked)
 btn.grid(column=1, row=0)
